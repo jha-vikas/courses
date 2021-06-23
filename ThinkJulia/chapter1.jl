99c80c24f91527a4f4426cb191fdf29e509b92a0
jha-vikas <vikasjhanitk@gmail.com>
2021-06-24 01:22:45 +0530

    Some julia fun

---
+++ ThinkJulia/chapter1.jl
@@ -0,0 +1,17 @@
+message = "And now for something completely different"
+println(message)
+
+π = 17
+print(π)
+
+# to run a file from command terminal: /path/to/julia script-name.jl
+# to run from repl: include("path/to/script-name.jl")
+
+# Functions
+
+parse(Int64, "32")
+parse(Float64, "3.14159")
+parse(Int64, "Hello")
+
+trunc(Int64, 23234.3434343)
+
