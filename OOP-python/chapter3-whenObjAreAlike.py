b53e0aa8d489f4879b2e856b2a0fbae6527e5754
jha-vikas <vikasjhanitk@gmail.com>
2021-10-03 01:53:52 +0530

    Updated custom classes

--- OOP-python/chapter3-whenObjAreAlike.py
+++ OOP-python/chapter3-whenObjAreAlike.py
@@ -281,3 +281,80 @@ class House(Property):
         })
         return parent_init
     prompt_init = staticmethod(prompt_init)
+
+
+class Purchase:
+    def __init__(self, price='', taxes='', **kwargs):
+        super().__init__(**kwargs)
+        self.price = price
+        self.taxes = taxes
+    def display(self):
+        super().display()
+        print("PURCHASE DETAILS")
+        print("selling price: {}".format(self.price))
+        print("estimated taxes: {}".format(self.taxes))
+    def prompt_init():
+        return dict(
+        price=input("What is the selling price? "),
+        taxes=input("What are the estimated taxes? "))
+    prompt_init = staticmethod(prompt_init)
+
+class Rental:
+    def __init__(self, furnished='', utilities='',
+        rent='', **kwargs):
+        super().__init__(**kwargs)
+        self.furnished = furnished
+        self.rent = rent
+        self.utilities = utilities
+    def display(self):
+        super().display()
+        print("RENTAL DETAILS")
+        print("rent: {}".format(self.rent))
+        print("estimated utilities: {}".format(self.utilities))
+        print("furnished: {}".format(self.furnished))
+    def prompt_init():
+        return dict(
+        rent=input("What is the monthly rent? "),
+        utilities=input(
+        "What are the estimated utilities? "),
+        furnished = get_valid_input(
+        "Is the property furnished? ",
+        ("yes", "no")))
+    prompt_init = staticmethod(prompt_init)
+
+
+class HouseRental(Rental, House):
+    def prompt_init():
+        init = House.prompt_init()
+        init.update(Rental.prompt_init())
+        return init
+    prompt_init = staticmethod(prompt_init)
+
+class ApartmentRental(Rental, Apartment):
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Rental.prompt_init())
+        return init
+    prompt_init = staticmethod(prompt_init)
+
+class ApartmentPurchase(Purchase, Apartment):
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Purchase.prompt_init())
+        return init
+    prompt_init = staticmethod(prompt_init)
+
+class HousePurchase(Purchase, House):
+    def prompt_init():
+        init = House.prompt_init()
+        init.update(Purchase.prompt_init())
+        return init
+    prompt_init = staticmethod(prompt_init)
+
+class Agent:
+    def __init__(self):
+        self.property_list = []
+    def display_properties(self):
+        for property in self.property_list:
+            property.display()
+
