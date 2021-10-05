64e1bc2efcdf2fbddc823f3070b216b01eaa2a61
jha-vikas <vikasjhanitk@gmail.com>
2021-10-05 19:31:51 +0530

    Updated custom exceptions

--- OOP-python/chapter4-expectingTheUnexpected.py
+++ OOP-python/chapter4-expectingTheUnexpected.py
@@ -51,3 +51,61 @@ def funny_division3(anumber):
     except ValueError:
         print("No, No, not 13!")
         raise
+
+try:
+    raise ValueError("This is an argument")
+except ValueError as e:
+    print("The exception arguments were", e.args)
+
+
+import random
+some_exceptions = [ValueError, TypeError, IndexError, None]
+
+try:
+    choice = random.choice(some_exceptions)
+    print(f"raising {choice}")
+    if choice:
+        raise choice("An error")
+except ValueError:
+    print("Caught a ValueError")
+except TypeError:
+    print("Caught a TypeError")
+except Exception as e:
+    print("Caught some other error: %s" %
+        ( e.__class__.__name__))
+else:
+    print("This code called if there is no exception")
+finally:
+    print("This cleanup code is always called")
+
+
+##### Custom exceptions ##################
+class InvalidWithdrawal(Exception):
+    pass
+#raise InvalidWithdrawal("You don't have $50 in your account")
+
+class InvalidWithdrawal(Exception):
+    def __init__(self, balance, amount) -> None:
+        super().__init__(f"account doesn't have ${amount}")
+        self.amount = amount
+        self.balance = balance
+    
+    def overage(self):
+        return self.amount - self.balance
+
+try:
+    raise InvalidWithdrawal(25, 50)
+except InvalidWithdrawal as e:
+    print(f"I'm sorry, but your withdral is more than your balance by ${e.overage()}")
+
+
+
+# Exceptions can be used in other cases too, not just exceptional circustances
+# it can be also used to control the flow like if-else
+
+def divide_with_exception(number, divisor):
+    try:
+        print(f"{number} / {divisor} = {round(number / divisor, 1)}")
+    except ZeroDivisionError:
+        print(f"You can't divide by zero")
+
