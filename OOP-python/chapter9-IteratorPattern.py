8acf4f69aeaa6d5b69b5ddb83158542fa5adc9e0
jha-vikas <vikasjhanitk@gmail.com>
2021-10-24 13:21:01 +0530

    Iterator patterns updated

---
+++ OOP-python/chapter9-IteratorPattern.py
@@ -0,0 +1,58 @@
+# Iterator protocol
+class CapitalIterator:
+    def __init__(self, string) -> None:
+        self.words = [w.capitalize() for w in string.split()]
+        self.index = 0
+    
+    def __next__(self):
+        if self.index == len(self.words):
+            raise StopIteration()
+
+        word = self.words[self.index]
+        self.index += 1
+        return word
+
+    def __iter__(self):
+        return self
+
+
+class CapitalIterable:
+    def __init__(self, string) -> None:
+        self.string = string
+    
+    def __iter__(self):
+        return CapitalIterator(self.string)
+
+
+# Comprehensions
+from os import name
+import sys
+filename = sys.argv[1]
+
+with open(filename) as file:
+    header = file.readline().strip().split('\t')
+    contacts = [
+        dict(
+            zip(header, line.strip.split('\t'))
+        ) for line in file
+    ]
+
+
+from collections import namedtuple
+
+Book = namedtuple("Book", "author title genre")
+
+books = [
+    Book("Pratchett", "Nightwatch", "fantasy"),
+    Book("Pratchett", "Thief Of Time", "fantasy"),
+    Book("Le Guin", "The Dispossessed", "scifi"),
+    Book("Le Guin", "A Wizard Of Earthsea", "fantasy"),
+    Book("Turner", "The Thief", "fantasy"),
+    Book("Phillips", "Preston Diamond", "western"),
+    Book("Phillips", "Twice Upon A Time", "scifi"),
+    ]
+
+fantasy_authors = {b.author for b in books if b.genre == 'fantasy'}
+fantasy_titles = {b.title: b.author for b in books if b.genre == 'fantasy'}
+
+# Generator expressions
